9f8d2086aa620549e591e6da2087ad5a97930bf6
Paulo Alves <paulorsaf@gmail.com>
2022-07-21 21:01:25 +0200

    Storing file on temp folder

--- src/file-upload/strategies/base64-upload-to-file-name.strategy.ts
+++ src/file-upload/strategies/base64-upload-to-file-name.strategy.ts
@@ -1,6 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import * as fs from 'fs';
+import * as os from 'os';
 
 @Injectable()
 export class Base64FileUploadToFileStrategy implements CanActivate {
@@ -9,7 +10,8 @@ export class Base64FileUploadToFileStrategy implements CanActivate {
         const request = context.switchToHttp().getRequest();
         const body = request.body;
 
-        const fileName = randomUUID() + body.name;
+        const fileName = `${os.tmpdir()}${randomUUID()}-${body.name}`;
+        console.log('### filename', fileName)
         const buff = Buffer.from(body.file.replace(/^data:image\/png;base64,/, ""), 'base64');
         fs.writeFileSync(fileName, buff);
 
