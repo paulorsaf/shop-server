9dbb0695f7307a4d63685d7475e67dc51e040299
Paulo Alves <paulorsaf@gmail.com>
2022-07-21 10:04:14 +0200

    Adding logs to add product image command handler

--- src/modules/product-images/commands/add-product-image/add-product-image-command.handler.ts
+++ src/modules/product-images/commands/add-product-image/add-product-image-command.handler.ts
@@ -18,6 +18,8 @@ export class AddProductImageCommandHandler implements ICommandHandler<AddProduct
     ) {}
 
     async execute(command: AddProductImageCommand) {
+        console.log('###', command.image.path);
+        
         await this.verifyProductExistsAndBelongsToCompany(command);
 
         const savedImage = await this.saveFileOnStorage(command);
