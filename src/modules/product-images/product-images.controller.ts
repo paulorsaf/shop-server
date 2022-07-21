73c691d0f588618c61013e434edaa37af279c072
Paulo Alves <paulorsaf@gmail.com>
2022-07-21 13:36:01 +0200

    Saving multipart temporary file on tmp folder

--- src/modules/product-images/product-images.controller.ts
+++ src/modules/product-images/product-images.controller.ts
@@ -19,14 +19,12 @@ export class ProductImagesController {
   @UseGuards(JwtAdminStrategy)
   @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({
-      destination: './uploads', filename: (req, file, cb) => {
+      destination: './tmp', filename: (req, file, cb) => {
         const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('')
         cb(null, `${randomName}${extname(file.originalname)}`)
       }
     })
   }))
-
-  @UseGuards(JwtAdminStrategy)
   @Post()
   add(
     @AuthUser() user: User,
